0d4fab2c892ff356e5a3229c38620b39e6647847
moraesjeremias <moraesjeremias@gmail.com>
2020-06-05 12:00:56 -0300

    Add Redis cached layer to queries

--- src/controller/redisController.js
+++ src/controller/redisController.js
@@ -1,37 +1,63 @@
 const fs = require('fs')
 const path = require('path')
+const redis = require('redis');
+const fakeDebts = require('../tmp/debtsMock')
+// const checkVehicleInfo = require('../services/checkVehicleInfo')
+
+const debitsClient = redis.createClient();
+
 
 module.exports = {
     status(request, response) {
-        return response.json('Routes working')
-    },
-
-
-    chachedDebits() {
-
-    },
-
-    retrieveDebts(request, response) {
-        const { placa, renavam, auth_token } = request.body;
-        console.log(placa, renavam, auth_token)
-        if (placa == 'LSO6I92' && renavam == '01089855947' && auth_token == 'abc') {
+        name = 'name'
+        debitsClient.get(name, (err, reply) => {
             try {
-                fs.readFile(path.resolve(__dirname, '..', 'tmp', 'debits.json'), 'utf8', function (err, data) {
-                    response.writeHead(200, { 'Content-Type': 'application/json' });
-                    response.write(data)
-                    console.log(data)
-                    return response.end().json()
-                })
-
+                return response.json(reply)
+            } catch (error) {
+                console.log(err)
+                return response.send(err)
             }
-            catch (error) {
-                console.log(error)
-                return error
-            }
-
-        }
-        // return response.json(placa)
+        })
     },
+
+    chachedDebts(request, response) {
+        const { placa, renavam, auth_token } = request.body;
+        debitsClient.get(`placa:${placa}`, (err, reply) => {
+            if(reply != null){
+                const jsonParsedRedisReply = JSON.parse(reply)
+                console.log('\n Retorno da Consulta no Redis: \n', jsonParsedRedisReply)
+                return response.json(jsonParsedRedisReply)
+            }else{
+                const databaseDebts = fakeDebts.findLicensePlate(placa,renavam)
+                const stringParsedDbDebts = JSON.stringify(databaseDebts);
+                console.log('\n Retorno da consulta no mock: \n', databaseDebts);
+                debitsClient.setex(`placa:${placa}`, 5, stringParsedDbDebts)
+                return response.json(databaseDebts) 
+            }  
+        })
+    },
+
+    retrieveAllDebts(request, response) {
+        try {
+            const debtsResults = fakeDebts.find()
+            console.log(debtsResults)
+            return response.json(debtsResults)
+        } catch (error) {
+            console.log(error)
+            return response.json(error)
+        }
+    },
+
+    retrieveQueriedDebt(request, response) {
+        const { placa, renavam } = request.body
+        try {
+            const queriedDebtResult = fakeDebts.findLicensePlate(placa, renavam)
+            return response.json(queriedDebtResult)
+        } catch (error) {
+            console.log(error)
+            return response.json(error)
+        }
+    }
 
 
 }
